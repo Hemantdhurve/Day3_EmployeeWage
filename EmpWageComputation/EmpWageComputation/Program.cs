d66f9231740f4f1a6a7dc5e9fdb81e6fde238682
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-14 17:29:01 +0530

    initial Commit

--- EmpWageComputation/EmpWageComputation/Program.cs
+++ EmpWageComputation/EmpWageComputation/Program.cs
@@ -9,11 +9,7 @@ namespace EmpWageComputation
 
             Console.WriteLine("Welcome to EmpWageComputation problem");
 
-            //so my class is static no need to instantiate/create object of class
-            //directly call method
-
-            Console.WriteLine("Output of Total Employee wage for a Month  :");
-            EmployeeWage.EmpWageTotalWorkingDays();
+           
         }
     }
 }
